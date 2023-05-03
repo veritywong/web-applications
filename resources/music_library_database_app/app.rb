3e5b0231b2186d51e032d90bb44e830b9cd0584a
Verity <veritywong90@gmail.com>
2023-05-03 12:13:25 +0100

    test-driving CRUD routes challenge complete in music_library_database_app

--- resources/music_library_database_app/app.rb
+++ resources/music_library_database_app/app.rb
@@ -1,6 +1,6 @@
 # file: app.rb
-require 'sinatra'
-require "sinatra/reloader"
+require 'sinatra/base'
+require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative 'lib/album_repository'
 require_relative 'lib/artist_repository'
@@ -13,4 +13,52 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
+  get '/albums' do
+    repo = AlbumRepository.new
+    albums = repo.all
+
+    response = albums.map do |album|
+      album.title
+    end.join(', ')
+
+    return response
+  end
+
+  post '/albums' do
+    repo = AlbumRepository.new
+    new_album = Album.new
+
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+
+    repo.create(new_album)
+
+    return ''
+  end
+
+  get '/artists' do
+    repo = ArtistRepository.new
+    artists = repo.all
+
+    response = artists.map do |artist|
+      artist.name
+    end.join(', ')
+
+    return response
+  end
+
+  post '/artists' do
+    repo = ArtistRepository.new
+    new_artist = Artist.new
+
+    new_artist.name = params[:name]
+    new_artist.genre = params[:genre]
+  
+    repo.create(new_artist)
+
+    return ''
+  end
+
 end
--- resources/music_library_database_app/spec/integration/application_spec.rb
+++ resources/music_library_database_app/spec/integration/application_spec.rb
@@ -9,5 +9,75 @@ describe Application do
   # We need to declare the `app` value by instantiating the Application
   # class so our tests work.
   let(:app) { Application.new }
+  def reset_albums_table
+    seed_sql = File.read('spec/seeds/albums_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+  
+  describe AlbumRepository do
+    before(:each) do 
+      reset_albums_table
+    end
 
+    context 'GET /albums' do
+      it 'should return the list of albums' do
+        response = get('/albums')
+
+        expected_response = 'Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+        
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(expected_response)
+      end
+    end
+
+    context 'POST /albums' do
+      it 'should create a new albums' do
+        response = post('/albums', title: 'OK Computer', release_year: '1997', artist_id: '1')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        response = get('/albums')
+        expect(response.body).to include('OK Computer')
+      end
+   end
+
+ end
+
+  def reset_artists_table
+    seed_sql = File.read('spec/seeds/artists_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  describe ArtistRepository do
+    before(:each) do 
+      reset_artists_table
+    end
+
+    context 'GET /artists' do
+      it 'returns a list of artists' do
+        response = get('/artists')
+
+        expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone'
+        
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(expected_response)
+      end
+    end
+
+    context 'POST /artists' do
+      it 'should create a new artist' do
+        response = post('/artists', name: 'Beyonce', genre: 'Pop')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        response = get('/artists')
+        expect(response.body).to include('Beyonce')
+      end
+    end
+
+  end
 end
